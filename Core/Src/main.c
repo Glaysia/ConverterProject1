8ba65af92b9e58e7aa29987412833af366a1b38a
Harry Lee <harry261@naver.com>
2025-11-05 20:51:15 +0900

    UART 신호 파형 측정 완료

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -48,11 +48,13 @@ DMA_HandleTypeDef hdma_adc1;
 TIM_HandleTypeDef htim1;
 
 UART_HandleTypeDef huart2;
+UART_HandleTypeDef huart3;
 
 /* USER CODE BEGIN PV */
 static uint8_t uartRxBuffer[5];
 static uint8_t latestFrame[5];
 static volatile uint8_t frameReady;
+volatile uint8_t debug_data;
 
 /* USER CODE END PV */
 
@@ -63,6 +65,7 @@ static void MX_DMA_Init(void);
 static void MX_ADC1_Init(void);
 static void MX_TIM1_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_USART3_UART_Init(void);
 /* USER CODE BEGIN PFP */
 static void StartUartReception(void);
 static void ProcessProtocolFrame(const uint8_t *frame);
@@ -107,6 +110,7 @@ int main(void)
   MX_ADC1_Init();
   MX_TIM1_Init();
   MX_USART2_UART_Init();
+  MX_USART3_UART_Init();
   /* USER CODE BEGIN 2 */
   StartUartReception();
 
@@ -314,6 +318,39 @@ static void MX_USART2_UART_Init(void)
 }
 
 /**
+  * @brief USART3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART3_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART3_Init 0 */
+
+  /* USER CODE END USART3_Init 0 */
+
+  /* USER CODE BEGIN USART3_Init 1 */
+
+  /* USER CODE END USART3_Init 1 */
+  huart3.Instance = USART3;
+  huart3.Init.BaudRate = 1200;
+  huart3.Init.WordLength = UART_WORDLENGTH_8B;
+  huart3.Init.StopBits = UART_STOPBITS_1;
+  huart3.Init.Parity = UART_PARITY_NONE;
+  huart3.Init.Mode = UART_MODE_TX_RX;
+  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart3.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART3_Init 2 */
+
+  /* USER CODE END USART3_Init 2 */
+
+}
+
+/**
   * Enable DMA controller clock
   */
 static void MX_DMA_Init(void)
@@ -377,6 +414,7 @@ int __io_putchar(int ch)
 {
   /* Redirect STDOUT to USART2 for printf */
   HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+  // HAL_UART_Transmit(&huart3, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
   return ch;
 }
 
@@ -406,8 +444,10 @@ static void ProcessProtocolFrame(const uint8_t *frame)
   if (chk != expectedChk)
   {
     printf("CHK WARN: got 0x%02X expected 0x%02X\r\n", chk, expectedChk);
+    debug_data = 00u;
   }
-
+    debug_data = data;
+    
   if (cmd == '1')
   {
     HAL_GPIO_WritePin(LED2_harry_GPIO_Port, LED2_harry_Pin, GPIO_PIN_SET);
@@ -418,6 +458,11 @@ static void ProcessProtocolFrame(const uint8_t *frame)
     HAL_GPIO_WritePin(LED2_harry_GPIO_Port, LED2_harry_Pin, GPIO_PIN_RESET);
     printf("LD2 OFF\r\n");
   }
+  HAL_UART_Transmit(&huart3, (uint8_t *)&STX, 1, HAL_MAX_DELAY);
+  HAL_UART_Transmit(&huart3, (uint8_t *)&cmd, 1, HAL_MAX_DELAY);
+  HAL_UART_Transmit(&huart3, (uint8_t *)&data, 1, HAL_MAX_DELAY);
+  HAL_UART_Transmit(&huart3, (uint8_t *)&ETX, 1, HAL_MAX_DELAY);
+  HAL_UART_Transmit(&huart3, (uint8_t *)&chk, 1, HAL_MAX_DELAY);
 }
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
